04a328420056aade633f2bf73c92011f08c7cf38
Viktor Krylov <t1001@front.ru>
2017-02-28 00:15:51 +0300

    ISSUE-4 - add config library

--- src/main/scala/su/t1001/daika/EntryPoint.scala
+++ src/main/scala/su/t1001/daika/EntryPoint.scala
@@ -1,8 +1,10 @@
 package su.t1001.daika
 
+import com.typesafe.config.ConfigFactory
 import org.slf4j.LoggerFactory
 
 object EntryPoint extends App {
   val log = LoggerFactory getLogger getClass
-  log debug "Hello"
+  val param = ConfigFactory.load().getString("app.param")
+  log debug s"Hello $param"
 }
